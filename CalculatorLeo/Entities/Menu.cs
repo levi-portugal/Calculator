6a8fcc1eaf8fb038cf655fd9c76a4bbb1aa39303
Levi <levi.portugal@sieg.com>
2026-03-04 14:51:05 -0300

    "Refactoring the classes, creating an interactive menu, and creating the history class."

--- CalculatorLeo/Entities/Menu.cs
+++ CalculatorLeo/Entities/Menu.cs
@@ -1,13 +1,42 @@
-﻿using System;
+﻿using CalculatorLeo.Entities;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
-namespace CalculatorLeo.Entities
+namespace TestCalculator.Entities
 {
-    internal class Menu
+    public class Menu : Operation
     {
-        public void HomeMenu() { 
-        
+        public void ShowMenu()
+        {
+            Console.Clear();
+            Menu showMenu = new();
+
+            Console.WriteLine("###-Virtual Calculator Menu-###\n");
+            Console.WriteLine("Choose a feature\n");
+            Console.WriteLine("New calc - enter 1 \nView Historic - enter 2 \nExit - enter 0 \n");
+
+            int resposta = int.Parse(Console.ReadLine());
+
+            switch (resposta)
+            {
+                case 1:
+                    Newcalc();
+                    break;
+                case 2:
+                    Historic h = new Historic();
+                    h.HistoricShow();
+                    break;
+                case 0:
+                    Console.WriteLine("Until later!");
+                    Environment.Exit(1);
+                    break;
+                default:
+                    Console.WriteLine("This option does not exist!");
+                    break;
+            }
+
         }
     }
 }
